0fe1a81edf3d5957588585b8307458043706038b
NithinReddy.T <96722265+Nithin123t@users.noreply.github.com>
2025-04-08 11:42:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './styles/main.css';
 import { registerSW } from "virtual:pwa-register";
 
 // Call it separately (not inside render)
-registerSW();
+registerSW({immediate : true});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
